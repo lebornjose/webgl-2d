6de1344a015342cb50acf636b7578bf1893f4a2a
saulhuang <xing.hx@mogic.ai>
2023-08-17 14:10:42 +0800

    feat: 滤镜；

---
+++ src/view/filter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>使用webgl 实现视频的裁剪功能</div>
+    <canvas id="webgl"></canvas>
+    <div class="flex">
+      <button @click="play">播放</button>
+      <button @click="pause">播放</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+let video: HTMLVideoElement
+const play = () => {
+    video.play();
+}
+const pause = () => {
+  video.pause();
+}
+
+</script>
