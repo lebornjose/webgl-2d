8812a64f13bd4552d51780e7ccde88e950b43ee5
saulhuang <xing.hx@mogic.ai>
2023-08-18 18:36:44 +0800

    feat: 滤镜实现播放

--- src/view/filter.vue
+++ src/view/filter.vue
@@ -93,6 +93,19 @@ const loadTexture = (gl, url) => {
   })
 }
 
+let texture = null
+
+const updateVideoTexture = (gl) => {
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
+  // 可选的纹理参数设置
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+}
+
 onMounted(async () => {
     const canvas: any = document.getElementById('webgl');
     canvas.width = 360;
@@ -109,75 +122,48 @@ onMounted(async () => {
     video.loop = false;
     video.setAttribute("crossOrigin", 'Anonymous');
 
+    initShaders(gl, vertexShaderSource, fragmentShaderSource);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    const source = new Float32Array([1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0]);
+    const sourceBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, sourceBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, source, gl.STATIC_DRAW);
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    // 方法绑定当前缓冲区范围到gl.ARRAY_BUFFER,成为当前顶点缓冲区对象的通用顶点属性并指定它的布局
+    gl.vertexAttribPointer(
+        a_Position,
+        2,
+        gl.FLOAT,
+        false,
+        0,
+        0
+    );
+    // 无论怎样，都需要你使用enableVertexAttribArray()方法，来激活每一个属性以便使用，不被激活的属性是不会被使用的。一旦激活，以下其他方法就可以获取到属性的值了
+    gl.enableVertexAttribArray(a_Position);
+    const texCoordLocation = gl.getAttribLocation(gl.program, 'a_texCoord');
+    gl.vertexAttribPointer(
+        texCoordLocation,
+        2,
+        gl.FLOAT,
+        false,
+        0,
+        0
+    );
+
+    gl.enableVertexAttribArray(texCoordLocation);
+    const opacity = gl.getUniformLocation(gl.program, 'opacity');
+    gl.uniform1f(opacity, 0.5)
+    /* 图像预处理 */
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
     /* 建立video对象 */
     const render = async() => {
-        console.log('111')
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
-        if (!video.paused) {
-            requestAnimationFrame(render);
-        }
-   }
-   video.addEventListener('loadeddata', function (e) {
-      video.currentTime = 0;
-      setTimeout(async () => {
-        initShaders(gl, vertexShaderSource, fragmentShaderSource);
-        // 给画布填充色
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        const source = new Float32Array([1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0]);
-        const sourceBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, sourceBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, source, gl.STATIC_DRAW);
-        const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-        // 方法绑定当前缓冲区范围到gl.ARRAY_BUFFER,成为当前顶点缓冲区对象的通用顶点属性并指定它的布局
-        gl.vertexAttribPointer(
-            a_Position,
-            2,
-            gl.FLOAT,
-            false,
-            0,
-            0
-        );
-        // 无论怎样，都需要你使用enableVertexAttribArray()方法，来激活每一个属性以便使用，不被激活的属性是不会被使用的。一旦激活，以下其他方法就可以获取到属性的值了
-        gl.enableVertexAttribArray(a_Position);
-    
-        const texCoordLocation = gl.getAttribLocation(gl.program, 'a_texCoord');
-        gl.vertexAttribPointer(
-            texCoordLocation,
-            2,
-            gl.FLOAT,
-            false,
-            0,
-            0
-        );
-    
-        gl.enableVertexAttribArray(texCoordLocation);
-        const opacity = gl.getUniformLocation(gl.program, 'opacity');
-        gl.uniform1f(opacity, 0.5)
-        /* 图像预处理 */
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
-        /* 准备三个角色 */
-        const texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(
-            gl.TEXTURE_2D,
-            0,
-            gl.RGB,
-            gl.RGB,
-            gl.UNSIGNED_BYTE,
-            video
-        )
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        texture = gl.createTexture();
+        updateVideoTexture(gl) 
         /* 获取uniform变量 */
         const u_image = gl.getUniformLocation(gl.program, 'u_image');
-
         const displacementMap = gl.getUniformLocation(gl.program, 'displacementMap');
-
-        const lutTexture = await loadTexture(gl, './filter/lut2.png');
-
+        const lutTexture = await loadTexture(gl, './filter/lut4.png');
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
 
@@ -186,12 +172,23 @@ onMounted(async () => {
 
         gl.uniform1i(u_image, 0);
         gl.uniform1i(displacementMap, 1);
-    
-        render();
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+        // gl.drawArrays(gl.TRIANGLES, 0, 6);
+        if (!video.paused) {
+            requestAnimationFrame(render);
+        }
+   }
+   video.addEventListener('loadeddata', function (e) {
+      video.currentTime = 0;
+      setTimeout(async () => {
+        // 给画布填充色
+        /* 准备三个角色 */
+        render()
+        // render(lutTexture);
       }, 50);
    })
-   video.addEventListener('playing', () => {
-     render();
+   video.addEventListener('playing', async () => {
+      render()
    })
 })
 </script>
