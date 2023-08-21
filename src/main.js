249fae54a7ec3899dddb7d217ab084bdb125ba2c
saulhuang <xing.hx@mogic.ai>
2023-08-21 10:35:21 +0800

    feat: color

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import { setupRouter } from './router';
 import App from './App.vue'
+import "ant-design-vue/dist/antd.css";
 
+import { DatePicker } from "ant-design-vue";
 
 
 const app = createApp(App)
+app.use(DatePicker);
 setupRouter(app)
 app.mount('#app')
