3bcd92e431d1b536d3b96a2e5d12b64cb403034b
saulhuang <xing.hx@mogic.ai>
2023-08-27 19:52:49 +0800

    feat: mask

--- src/view/mask.vue
+++ src/view/mask.vue
@@ -1,5 +1,12 @@
 <template>
     <h3>使用webgl 实现画面的蒙版功能</h3>
+    <div class="main">
+      <div class="left-canvas">
+        <canvas id="webgl"></canvas>
+      </div>
+    </div>  
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+
+</script>
