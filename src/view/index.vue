136260fc0fcb64db091fbe3333939f8bfa0592b6
saulhuang <xing.hx@mogic.ai>
2024-09-04 13:10:26 +0800

    feat: 导航

--- src/view/index.vue
+++ src/view/index.vue
@@ -5,26 +5,33 @@
       <div class="container">
         <h2>音视频demo</h2>
         <div class="list">
-            <a-button type="primary">转场</a-button>
-            <a-button type="primary">动画</a-button>
-            <a-button type="primary">蒙版</a-button>
-            <a-button type="primary">视频色值调整</a-button>
-            <a-button type="primary">九宫格特效</a-button>
-            <a-button type="primary">视频裁剪</a-button>
-            <a-button type="primary">画面蒙版</a-button>
-            <a-button type="primary">平移, 缩放，旋转</a-button>
+            <a-button type="primary" @click="toUrl('transition')">转场</a-button>
+            <a-button type="primary" @click="toUrl('in_animation')">出场动画</a-button>
+            <a-button type="primary" @click="toUrl('flip')">视频翻转</a-button>
+            <a-button type="primary" @click="toUrl('mask')">蒙版</a-button>
+            <a-button type="primary" @click="toUrl('cir_mask')">圆形蒙版</a-button>
+            <a-button type="primary" @click="toUrl('color')">视频色值调整</a-button>
+            <a-button type="primary" @click="toUrl('effect')">九宫格特效</a-button>
+            <a-button type="primary" @click="toUrl('crop')">视频裁剪</a-button>
+            <a-button type="primary" @click="toUrl('matrix')">平移, 缩放，旋转</a-button>
         </div>
         <h2>webCodecs</h2>
         <div class="list">   
-            <a-button type="primary">视频解码播放</a-button>
-            <a-button type="primary">视频关键帧</a-button>
-            <a-button type="primary">解码帧序列图</a-button>
-            <a-button type="primary">音画分离</a-button>
+            <a-button type="primary" @click="toUrl('WebAv')">视频解码播放</a-button>
+            <a-button type="primary" @click="toUrl('clip')">视频关键帧</a-button>
+            <a-button type="primary" @click="toUrl('webAvFrame')">解码帧序列图</a-button>
+            <a-button type="primary" @click="toUrl('webAvSpilt')">音画分离</a-button>
         </div>
       </div>
    </div>
 </template>
+<script type="ts" setup>
+import { router } from '../router';
 
+const toUrl = (name) => {
+    router.push({ name })
+}
+</script>
 <style lang="css" scoped>
 .container{
     width: 1000px;
