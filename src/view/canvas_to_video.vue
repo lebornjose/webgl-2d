08b7e30c4b2e354f7c104263c663e0dbaca5c731
saulhuang <xing.hx@mogic.ai>
2024-10-11 13:17:02 +0800

    feat: canvas 导出为视频

---
+++ src/view/canvas_to_video.vue
@@ -0,0 +1,23 @@
+
+<template>
+   <div class="content">
+      <h2>canvas 导出为视频</h2>
+      <div class="list">
+
+      </div>
+      <canvas></canvas>
+      <div
+        ref="initCvs"
+        class="video-container"
+      >
+         <!-- <video src="/public/video/output.mp4" ref="videoRef" muted loop autoplay/> -->
+      </div>
+   </div>
+</template>
+
+<style lang="css" scoped>
+.content{
+    width: 800px;
+    margin: 40px auto;
+}
+</style>
