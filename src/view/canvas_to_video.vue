89e78714b57505168c260352525ab5c1817a4cfd
saulhuang <xing.hx@mogic.ai>
2024-10-11 13:23:56 +0800

    feat: start

--- src/view/canvas_to_video.vue
+++ src/view/canvas_to_video.vue
@@ -3,7 +3,7 @@
    <div class="content">
       <h2>canvas 导出为视频</h2>
       <div class="list">
-
+        <a-button @click="start">启动</a-button>
       </div>
       <canvas></canvas>
       <div
@@ -15,6 +15,11 @@
    </div>
 </template>
 
+<script lang="ts" setup>
+    const start = () => {
+        console.log('start')
+    }
+</script>
 <style lang="css" scoped>
 .content{
     width: 800px;
